722f8063d0763d4cae8c9a4724c35d3d6a0a0d54
jimena-garcia <a19203731@alumnos.uady.mx>
2024-12-30 23:56:08 -0600

    Corregir la redirección a 'Index.html' en la función de redirección

--- sobre.js
+++ sobre.js
@@ -5,7 +5,7 @@
  function redirectToIndex() {
    document.querySelector('.envelope-container').classList.add('fade-out');
    setTimeout(function () {
-     window.location.href = 'index.html';
+     window.location.href = 'Index.html';
    }, 1000); // Tiempo en milisegundos, debe coincidir con la duración de la transición
  }
 
